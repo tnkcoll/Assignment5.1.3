ac4b5b1f85d039bb77f50ca2b561da6aff1875b5
tnkcoll <theodore.collins2023@outlook.com>
2024-06-03 13:27:22 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Assignment5._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = GetNumber("Please enter a multi-digit number");
+            Console.WriteLine(DuplicateNumbers(number));
+            
+        }
+
+        static string GetNumber(string s)
+        {
+            string? userInput = "";
+            do
+            {
+                Console.WriteLine(s);
+                userInput = Console.ReadLine();
+            } while (userInput == "");
+            return userInput;
+        }
+
+        static bool DuplicateNumbers(string num)
+        {
+            for (int i = 0; i < num.Length - 1; i++)
+            {
+                for (int j = i + 1; j < num.Length; j++)
+                {
+                    if (num[j] == num[i])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
